fc7f7896d93e6fe92d48cec2d85a3b99d14381ab
KarloSs428 <demelkaja@seznam.cz>
2019-05-02 21:57:12 +0200

    blablaa

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ $(function(){
     $('.objekt').on('mouseenter',function(){
         $(this).attr({'style':'fill:rgba(0,255,0,0.15);stroke:rgba(0,255,0,0.15)'});
         $('#text h2').text($(this).data('nazev'));
-        $('#text p').text($(this).data('obrazek'));
+        $('#upgrade1').attr('src',$(this).data('upgrade1'));
+        $('#upgrade2').attr('src',$(this).data('upgrade2'));
+        $('#upgrade3').attr('src',$(this).data('upgrade3'));
     })
     $('.objekt').on('mouseleave',function(){
         $(this).attr({'style':'fill:rgba(0,255,0,0);stroke:rgba(0,255,0,0)'});
